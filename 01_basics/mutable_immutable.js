6f256c303c8b381fb609ecd7bc88f2e6b7f13c52
Rajiv Sharma <sharma9769387961@gmail.com>
2025-02-07 16:29:23 +0530

    added mutable and immutable

---
+++ 01_basics/mutable_immutable.js
@@ -0,0 +1,14 @@
+// Reference link
+// https://dev.to/mrizwanashiq/primitive-and-non-primitive-56n8
+
+let userOne = {         // The reference to this object is stored in the Stack.
+    email: "user@google.com",
+    upi: "user@ybl"
+};                      // The actual object data is stored in the Heap.
+
+let userTwo = userOne;  // userTwo references the same object in the Heap.
+
+userTwo.email = "ayush@google.com"; // Modifying userTwo also affects userOne.
+
+console.log(userOne.email); // Output: ayush@google.com
+console.log(userTwo.email); // Output: ayush@google.com

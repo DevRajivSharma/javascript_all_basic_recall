34ce4f4d51ddd48e1b660548479a3da9090869a6
Rajiv Sharma <sharma9769387961@gmail.com>
2025-02-21 23:25:07 +0530

    added more detailed explanation to class getter,setter

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,14 +1,20 @@
 class User {
+
+    /*
+      Here when we want to define a property which need to acces through
+      getter and setter than it should be start with "_" [recommended]
+      since getter and setter propery name will be use as property
+    */
     constructor(email, password){
         this.email = email;
         this.password = password
     }
 
-    get email(){
-        return this._email.toUpperCase()
+    get getemail(){
+        return this.email.toUpperCase()
     }
-    set email(value){
-        this._email = value
+    set setemail(value){
+        this.email = value
     }
 
     get password(){
@@ -20,5 +26,6 @@ class User {
     }
 }
 
-const hitesh = new User("h@hitesh.ai", "abc")
-console.log(hitesh.email);
+const rajiv = new User("rajiv@netflix.com", "abc")
+//hitesh.setemail = "rajiv@netflix.com"
+console.log(rajiv.getemail)
--- 10_classes_and_oop/private_var_class.js
+++ 10_classes_and_oop/private_var_class.js
@@ -1,6 +1,6 @@
 class MyClass {
   #privateVariable; // This is private property '#' is used followed by properties name to make it private [Acces only inside the class]
-  static root_val = "Rajiv"; // This is Myclass specific property only and cannot be called by its instance
+  static root_val = "Rajiv"; // Belons to MyCllas only and cannot be called by its instance
   constructor(value) {
     this.#privateVariable = value;
     this.name = value;

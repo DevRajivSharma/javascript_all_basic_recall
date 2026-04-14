2226a92d5cf9f2e1a90c359189e04bae3ccdbfba
DevRajivSharma <sharma9769387961@gmail.com>
2026-04-14 17:14:18 +0530

    refactor: simplify event binding and fix object iteration
    - Remove unnecessary .bind() call in event listener as arrow function in constructor already binds context
    - Fix for...of loop to iterate over Object.entries(chai) instead of directly over chai object
    - Comment out unused code for better clarity

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -19,14 +19,14 @@ const chai = {
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
 Object.defineProperty(chai, 'name', {
-    //writable: false,
+//    writable: false,
     enumerable: true,
     
 })
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
-
-for (let [key, value] of Object.entries(chai)) {
+//console.log(Object.entries(chai))
+for (let [key, value] of chai) {
     if (typeof value !== 'function') {
         
         console.log(`${key} : ${value}`);
